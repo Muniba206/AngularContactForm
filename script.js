bff3ea22f594afc9c62dae5da8874927a6735e6b
Muniba206 <Muniba.Talha@yahoo.co.uk>
2017-04-14 17:10:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ var app = angular.module("contact",[]);
 app.controller("contactCtrl", function($scope){
 
   // validating phone number
-  $scope.nums = /^\+?\d{8,10}|\d{3}-\d{3}-\d{4}|\d{2}\s\d{2}\s\d{2}\s\d{2}|\d{4}\s\d{4}|\d{4}-\d{2}\s\d{2}\s\d{2}|\+\d{2}\s\d{3}-\d{3}-\d{3}|\d{2}-\d{3}\s\d{2}\s\d{2}$/;
-  $scope.email= /^\S+@\S+\.\S+$/;
+  $scope.nums = /^[1-9]{1}[0-9]{7}$/;
   $scope.submitForm = function(isValid) {
-    alert("Form has been submitted")
+  //  alert("Form has been submitted")
 
   };
 });
