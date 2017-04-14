8661f9b3227a0e345272a75e9889270ba3e6a633
Muniba206 <Muniba.Talha@yahoo.co.uk>
2017-04-14 22:53:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ app.controller("contactCtrl", function($scope){
 
   // validating phone number
   $scope.nums = /^[1-9]{1}[0-9]{7}$/;
+  $scope.master = {};
+
+    $scope.reset = function() {
+      $scope.list = angular.copy($scope.master);
+    };
+
+    $scope.reset();
+}
   $scope.submitForm = function(isValid) {
   //  alert("Form has been submitted")
 
